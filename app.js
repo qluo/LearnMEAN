085e916ac528d919b3419e626d015b3e6bf3f55d
Q. Luo <luoql1986@gmail.com>
2015-03-22 23:05:18 -0400

    Displaying Lists

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-var app = angular.module('flapperNews'. []);
+var app = angular.module('flapperNews', []);
 
 app.controller('MainCtrl', [
-'$scope', fucntion($scope) {
+'$scope',
+function($scope) {
     $scope.text = 'Hello world!';
+    $scope.posts = [
+      {title:'post 1', upvotes: 5},
+      {title:'post 2', upvotes: 2},
+      {title:'post 3', upvotes: 15},
+      {title:'post 4', upvotes: 9},
+      {title:'post 5', upvotes: 4}
+    ];
 }
 ]);
